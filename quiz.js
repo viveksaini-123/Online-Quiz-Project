8d64d7bb279b45a7403777a6c5e82d20074610b5
viveksaini-123 <viveksainirohtagi7456@gmail.com>
2025-01-18 19:51:54 +0530

    adding files

---
+++ quiz.js
@@ -0,0 +1,602 @@
+/****Reservoir Array for all values************/
+
+var ar = [];
+
+// /***********Question one*********************/
+
+// //QuestOneColor(),QuestTwoColor() etc methods are used to bring color when we click on options
+
+function QuestOneColor() {
+// light blue darkorange
+    document.getElementById("1A").style.backgroundColor = "black";
+    document.getElementById("1B").style.backgroundColor = "grey";
+    document.getElementById("1C").style.backgroundColor = "purple";
+
+}
+
+
+function QuestOneOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("1A").disabled = true;
+    document.getElementById("1B").disabled = true;
+    document.getElementById("1C").disabled = true;
+}
+
+function QuestOneZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("1A").disabled = true;
+    document.getElementById("1B").disabled = true;
+    document.getElementById("1C").disabled = true;
+
+}
+
+
+/***********Question Two*********************/
+
+function QuestTwoColor() {
+
+    document.getElementById("2A").style.backgroundColor = "lightgreen";
+    document.getElementById("2B").style.backgroundColor = "darkorange";
+    document.getElementById("2C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestTwoOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("2A").disabled = true;
+    document.getElementById("2B").disabled = true;
+    document.getElementById("2C").disabled = true;
+}
+
+function QuestTwoZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("2A").disabled = true;
+    document.getElementById("2B").disabled = true;
+    document.getElementById("2C").disabled = true;
+
+}
+
+
+
+/***********Question Three*********************/
+
+function QuestThreeColor() {
+
+    document.getElementById("3A").style.backgroundColor = "lightgreen";
+    document.getElementById("3B").style.backgroundColor = "darkorange";
+    document.getElementById("3C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestThreeOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("3A").disabled = true;
+    document.getElementById("3B").disabled = true;
+    document.getElementById("3C").disabled = true;
+}
+
+function QuestThreeZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("3A").disabled = true;
+    document.getElementById("3B").disabled = true;
+    document.getElementById("3C").disabled = true;
+
+}
+
+
+
+/***********Question Four*********************/
+
+function QuestFourColor() {
+
+    document.getElementById("4A").style.backgroundColor = "lightgreen";
+    document.getElementById("4B").style.backgroundColor = "darkorange";
+    document.getElementById("4C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestFourOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("4A").disabled = true;
+    document.getElementById("4B").disabled = true;
+    document.getElementById("4C").disabled = true;
+}
+
+function QuestFourZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("4A").disabled = true;
+    document.getElementById("4B").disabled = true;
+    document.getElementById("4C").disabled = true;
+
+}
+
+
+
+/***********Question Five*********************/
+
+function QuestFiveColor() {
+
+    document.getElementById("5A").style.backgroundColor = "lightgreen";
+    document.getElementById("5B").style.backgroundColor = "darkorange";
+    document.getElementById("5C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestFiveOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("5A").disabled = true;
+    document.getElementById("5B").disabled = true;
+    document.getElementById("5C").disabled = true;
+}
+
+function QuestFiveZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("5A").disabled = true;
+    document.getElementById("5B").disabled = true;
+    document.getElementById("5C").disabled = true;
+
+}
+
+
+
+/***********Question Six*********************/
+
+function QuestSixColor() {
+
+    document.getElementById("6A").style.backgroundColor = "lightgreen";
+    document.getElementById("6B").style.backgroundColor = "darkorange";
+    document.getElementById("6C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestSixOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("6A").disabled = true;
+    document.getElementById("6B").disabled = true;
+    document.getElementById("6C").disabled = true;
+}
+
+function QuestSixZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("6A").disabled = true;
+    document.getElementById("6B").disabled = true;
+    document.getElementById("6C").disabled = true;
+
+}
+
+
+
+/***********Question Seven*********************/
+
+function QuestSevenColor() {
+
+    document.getElementById("7A").style.backgroundColor = "lightgreen";
+    document.getElementById("7B").style.backgroundColor = "darkorange";
+    document.getElementById("7C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestSevenOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("7A").disabled = true;
+    document.getElementById("7B").disabled = true;
+    document.getElementById("7C").disabled = true;
+}
+
+function QuestSevenZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("7A").disabled = true;
+    document.getElementById("7B").disabled = true;
+    document.getElementById("7C").disabled = true;
+
+}
+
+
+
+/***********Question Eight*********************/
+
+function QuestEightColor() {
+
+    document.getElementById("8A").style.backgroundColor = "lightgreen";
+    document.getElementById("8B").style.backgroundColor = "darkorange";
+    document.getElementById("8C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestEightOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("8A").disabled = true;
+    document.getElementById("8B").disabled = true;
+    document.getElementById("8C").disabled = true;
+}
+
+function QuestEightZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("8A").disabled = true;
+    document.getElementById("8B").disabled = true;
+    document.getElementById("8C").disabled = true;
+
+}
+
+
+
+/***********Question Nine*********************/
+
+function QuestNineColor() {
+
+    document.getElementById("9A").style.backgroundColor = "lightgreen";
+    document.getElementById("9B").style.backgroundColor = "darkorange";
+    document.getElementById("9C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestNineOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("9A").disabled = true;
+    document.getElementById("9B").disabled = true;
+    document.getElementById("9C").disabled = true;
+}
+
+function QuestNineZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("9A").disabled = true;
+    document.getElementById("9B").disabled = true;
+    document.getElementById("9C").disabled = true;
+
+}
+
+
+
+/***********Question Ten*********************/
+
+function QuestTenColor() {
+
+    document.getElementById("10A").style.backgroundColor = "lightgreen";
+    document.getElementById("10B").style.backgroundColor = "darkorange";
+    document.getElementById("10C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestTenOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("10A").disabled = true;
+    document.getElementById("10B").disabled = true;
+    document.getElementById("10C").disabled = true;
+}
+
+function QuestTenZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("10A").disabled = true;
+    document.getElementById("10B").disabled = true;
+    document.getElementById("10C").disabled = true;
+
+}
+
+
+
+/***********Question Eleven*********************/
+
+function QuestElevenColor() {
+
+    document.getElementById("11A").style.backgroundColor = "lightgreen";
+    document.getElementById("11B").style.backgroundColor = "darkorange";
+    document.getElementById("11C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestElevenOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("11A").disabled = true;
+    document.getElementById("11B").disabled = true;
+    document.getElementById("11C").disabled = true;
+}
+
+function QuestElevenZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("11A").disabled = true;
+    document.getElementById("11B").disabled = true;
+    document.getElementById("11C").disabled = true;
+
+}
+
+
+
+/***********Question Twelve*********************/
+
+function QuestTwelveColor() {
+
+    document.getElementById("12A").style.backgroundColor = "lightgreen";
+    document.getElementById("12B").style.backgroundColor = "darkorange";
+    document.getElementById("12C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestTwelveOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("12A").disabled = true;
+    document.getElementById("12B").disabled = true;
+    document.getElementById("12C").disabled = true;
+}
+
+function QuestTwelveZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("12A").disabled = true;
+    document.getElementById("12B").disabled = true;
+    document.getElementById("12C").disabled = true;
+
+}
+
+
+
+/***********Question Thirteen*********************/
+
+function QuestThirteenColor() {
+
+    document.getElementById("13A").style.backgroundColor = "lightgreen";
+    document.getElementById("13B").style.backgroundColor = "darkorange";
+    document.getElementById("13C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestThirteenOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("13A").disabled = true;
+    document.getElementById("13B").disabled = true;
+    document.getElementById("13C").disabled = true;
+}
+
+function QuestThirteenZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("13A").disabled = true;
+    document.getElementById("13B").disabled = true;
+    document.getElementById("13C").disabled = true;
+
+}
+
+
+
+/***********Question Fourteen*********************/
+
+
+function QuestFourteenColor() {
+
+    document.getElementById("14A").style.backgroundColor = "lightgreen";
+    document.getElementById("14B").style.backgroundColor = "darkorange";
+    document.getElementById("14C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestFourteenOne(x) {
+    var x = 1;
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("14A").disabled = true;
+    document.getElementById("14B").disabled = true;
+    document.getElementById("14C").disabled = true;
+}
+
+function QuestFourteenZero(y) {
+    var y = 0;
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("14A").disabled = true;
+    document.getElementById("14B").disabled = true;
+    document.getElementById("14C").disabled = true;
+
+}
+
+
+//***********Question Fifteen*********************/
+
+
+function QuestFifteenColor() {
+
+    document.getElementById("15A").style.backgroundColor = "lightgreen";
+    document.getElementById("15B").style.backgroundColor = "darkorange";
+    document.getElementById("15C").style.backgroundColor = "lightgreen";
+
+}
+
+
+function QuestFifteenOne(x) {
+    var x = 1;  // 1 denotes a correct answee
+    console.log(x);
+    console.log(typeof x);
+    ar.push(x);
+    console.log(ar);
+    document.getElementById("15A").disabled = true;
+    document.getElementById("15B").disabled = true;
+    document.getElementById("15C").disabled = true;
+}
+
+function QuestFifteenZero(y) {
+    var y = 0;  // 0 denotes a wrong answer
+    console.log(y);
+    console.log(typeof y);
+    ar.push(y);
+    console.log(ar);
+    document.getElementById("15A").disabled = true;
+    document.getElementById("15B").disabled = true;
+    document.getElementById("15C").disabled = true;
+
+}
+
+
+
+//***********Submit Button*********************/
+
+function SubmitColor() {
+
+    document.getElementById("Submit").style.backgroundColor = "darkviolet";
+
+}
+
+function result(z) {
+    var sum = 0;
+    for (var i = 0; i < ar.length; i++)
+        sum += ar[i];
+
+    console.log(sum);
+
+    document.getElementById("Submit").disabled = true;
+    document.getElementById("Submit").innerHTML = 'Your Score is ' + sum + '/15!';
+
+
+
+}
+
+/*
+ 
+Great question! You are right to ask about the condition, because at first glance, there isn't an explicit if condition checking whether an answer is correct or incorrect in the result function. The reason this works and only counts correct answers (or adds 1s for correct answers) is because of how the array ar is populated.
+
+Here's how it works step by step:
+1. The ar array stores the answers
+In your code, the answers to each question are stored in the ar array as either 1 (correct answer) or 0 (incorrect answer). This is done by the functions like QuestOneOne() and QuestOneZero() when a user clicks an option.
+
+Example:
+
+javascript
+Copy
+function QuestOneOne(x) {
+    var x = 1;  // 1 denotes the correct answer
+    ar.push(x); // Adds 1 to the array
+}
+
+function QuestOneZero(y) {
+    var y = 0;  // 0 denotes an incorrect answer
+    ar.push(y); // Adds 0 to the array
+}
+When the user selects the correct answer, a 1 is pushed into the array ar.
+When the user selects an incorrect answer, a 0 is pushed into the array ar.
+So, in the ar array, you will have 1s and 0s like this:
+
+javascript
+Copy
+// Example of `ar` after the user has answered several questions:
+ar = [1, 0, 1, 0, 1]; // Correct, Incorrect, Correct, Incorrect, Correct
+2. The result() function doesn't need explicit conditions
+The result() function doesn’t need an if condition to check for correct or incorrect answers because the array ar already contains 1 for correct answers and 0 for incorrect answers.
+
+*/
